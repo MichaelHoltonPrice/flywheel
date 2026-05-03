1438a31ba273f81c28a3e1a1e29bb2ccca9c61d4
MichaelHoltonPrice <MichaelHoltonPrice@gmail.com>
2026-05-02 17:16:01 -0700

    Propagate pattern params through recursive invocations

--- flywheel/invocation.py
+++ flywheel/invocation.py
@@ -228,6 +228,7 @@ def dispatch_invocations(
                 state_validator_registry=state_validator_registry,
                 invoking_execution_id=parent_execution.id,
                 dispatch_child_invocations=True,
+                invocation_params=params,
                 run_context=run_context,
                 invocation_chain=invocation_chain,
                 max_invocation_depth=max_invocation_depth,
